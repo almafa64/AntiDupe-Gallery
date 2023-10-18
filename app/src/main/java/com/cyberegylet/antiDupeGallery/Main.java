16fa504f3a8a411255cce3f38abd0afbb450208b
almafa64 <bhatalyak@gmail.com>
2023-10-18 22:08:35 +0200

    Initial commit

---
+++ app/src/main/java/com/cyberegylet/antiDupeGallery/Main.java
@@ -0,0 +1,26 @@
+package com.cyberegylet.antiDupeGallery;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+import com.cyberegylet.antiDupeGallery.backend.Test;
+
+public class Main extends Activity
+{
+    static {
+        System.loadLibrary("gallery_backend");
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main_activity);
+
+        TextView a = findViewById(R.id.textView);
+        a.setText(String.valueOf(Test.test()));
+    }
+}
---
+++ app/src/main/java/com/cyberegylet/antiDupeGallery/backend/Test.java
@@ -0,0 +1,6 @@
+package com.cyberegylet.antiDupeGallery.backend;
+
+public class Test
+{
+    public static native int test();
+}
