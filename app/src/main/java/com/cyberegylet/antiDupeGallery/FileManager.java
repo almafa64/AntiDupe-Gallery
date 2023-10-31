b21a77b4b93c37642b08bd8ecde0fc84e7f58132
almafa64 <bhatalyak@gmail.com>
2023-11-01 00:17:46 +0100

    Files:     Fixed permissions for api level >33     Reworked CursorWrapper     Installed Glide for working with images -> made wrapper in FileManager UI:     Made simple gallery view increased min API level to 27

--- app/src/main/java/com/cyberegylet/antiDupeGallery/FileManager.java
+++ app/src/main/java/com/cyberegylet/antiDupeGallery/FileManager.java
@@ -7,13 +7,19 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.provider.MediaStore;
+import android.widget.ImageView;
 
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import java.io.File;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.Downsampler;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class FileManager
@@ -23,8 +29,37 @@ public class FileManager
 	private final ContentResolver contentResolver;
 
 	public static final int STORAGE_REQUEST_CODE = 1;
+	public static final Uri externalUri = MediaStore.Files.getContentUri("external");
 
 	private boolean has_read_access = false;
+
+	public static class Mimes
+	{
+		public static final String[] MIME_ALL = new String[]{ "video/quicktime", "video/mpeg", "video/mp4", "video/3gpp", "video/webm",
+				"video/avi", "image/jpeg", "image/png", "image/gif", "image/webp", "image/bmp", "image/ico", "image/svg", "audio/3gpp",
+				"audio/midi", "audio/mpeg", "audio/x-wav" };
+		public static final String[] MIME_VIDEOS = new String[]{ "video/quicktime", "video/mpeg", "video/mp4", "video/3gpp", "video/webm",
+				"video/avi" };
+		public static final String[] MIME_IMAGES = new String[]{ "image/jpeg", "image/png", "image/gif", "image/webp", "image/bmp",
+				"image/ico", "image/svg" };
+		public static final String[] MIME_AUDIOS = new String[]{ "audio/3gpp", "audio/midi", "audio/mpeg", "audio/x-wav" };
+
+		public enum Type
+		{
+			MIME_NONE,
+			MIME_IMAGE,
+			MIME_VIDEO,
+			MIME_AUDIO,
+		}
+
+		public static boolean isImage(String mime_string) { return Arrays.asList(MIME_IMAGES).contains(mime_string); }
+
+		public static boolean isAudio(String mime_string) { return Arrays.asList(MIME_AUDIOS).contains(mime_string); }
+
+		public static boolean isVideo(String mime_string) { return Arrays.asList(MIME_VIDEOS).contains(mime_string); }
+
+		public static boolean isMedia(String mime_string) { return Arrays.asList(MIME_ALL).contains(mime_string); }
+	}
 
 	public FileManager(Activity activity)
 	{
@@ -35,7 +70,14 @@ public class FileManager
 		if (ContextCompat.checkSelfPermission(context,
 				android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED)
 		{
-			ActivityCompat.requestPermissions(activity, new String[]{ Manifest.permission.READ_EXTERNAL_STORAGE }, STORAGE_REQUEST_CODE);
+			String[] request;
+			if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
+			{
+				request = new String[]{ Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.READ_MEDIA_IMAGES, Manifest.permission.READ_MEDIA_VIDEO, Manifest.permission.READ_MEDIA_AUDIO };
+			}
+			else request = new String[]{ Manifest.permission.READ_EXTERNAL_STORAGE };
+
+			ActivityCompat.requestPermissions(activity, request, STORAGE_REQUEST_CODE);
 		}
 		else has_read_access = true;
 	}
@@ -44,13 +86,24 @@ public class FileManager
 
 	public abstract static class CursorLoopWrapper
 	{
-		private int id_col, path_col;
+		private int id_col, path_col, mime_col;
+		private Cursor cursor;
 
-		public int getIdCol() { return id_col; }
+		protected void innit(Cursor cursor)
+		{
+			this.cursor = cursor;
+			id_col = cursor.getColumnIndex(MediaStore.MediaColumns._ID);
+			path_col = cursor.getColumnIndex(MediaStore.MediaColumns.DATA);
+			mime_col = cursor.getColumnIndex(MediaStore.MediaColumns.MIME_TYPE);
+		}
 
-		public int getPathCol() { return path_col; }
+		public int getID() { return cursor.getInt(id_col); }
 
-		public abstract void run(Cursor cursor);
+		public String getPath() { return cursor.getString(path_col); }
+
+		public String getMime() { return cursor.getString(mime_col); }
+
+		public abstract void run();
 	}
 
 	public void CursorLoop(CursorLoopWrapper wrapper, int cursorStart, String sort, String selection, Uri uri, String... queries)
@@ -58,12 +111,11 @@ public class FileManager
 		try (Cursor cursor = contentResolver.query(uri, queries, selection, null, sort))
 		{
 			assert cursor != null;
-			wrapper.id_col = cursor.getColumnIndex(MediaStore.MediaColumns._ID);
-			wrapper.path_col = cursor.getColumnIndex(MediaStore.MediaColumns.DATA);
+			wrapper.innit(cursor);
 			if (!cursor.moveToPosition(cursorStart)) return;
 			do
 			{
-				wrapper.run(cursor);
+				wrapper.run();
 			} while (cursor.moveToNext());
 		}
 	}
@@ -80,15 +132,21 @@ public class FileManager
 
 	public void CursorLoop(CursorLoopWrapper wrapper, Uri uri, String... queries) { CursorLoop(wrapper, null, uri, queries); }
 
+	public void allImageAndVideoLoop(String sort, CursorLoopWrapper wrapper, String... queries)
+	{
+		String selection = MediaStore.Files.FileColumns.MIME_TYPE + " like 'image/%' or " + MediaStore.Files.FileColumns.MIME_TYPE + " like 'video/%'";
+		CursorLoop(wrapper, sort, selection, externalUri, queries);
+	}
+
 	private List<Integer> getAllID(Uri uri)
 	{
 		List<Integer> ids = new ArrayList<>();
 		CursorLoop(new CursorLoopWrapper()
 		{
 			@Override
-			public void run(Cursor cursor)
+			public void run()
 			{
-				ids.add(cursor.getInt(getIdCol()));
+				ids.add(getID());
 			}
 		}, uri, MediaStore.MediaColumns._ID);
 		return ids;
@@ -100,9 +158,9 @@ public class FileManager
 		CursorLoop(new CursorLoopWrapper()
 		{
 			@Override
-			public void run(Cursor cursor)
+			public void run()
 			{
-				paths.add(cursor.getString(getPathCol()));
+				paths.add(getPath());
 			}
 		}, uri, MediaStore.MediaColumns.DATA);
 		return paths;
@@ -113,14 +171,16 @@ public class FileManager
 	public List<Integer> getAllVideoID() { return getAllID(MediaStore.Video.Media.EXTERNAL_CONTENT_URI); }
 
 	public List<Integer> getAllAudioID() { return getAllID(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI); }
-	public List<Integer> getAllFileID() { return getAllID(MediaStore.Files.getContentUri("external")); }
+
+	public List<Integer> getAllFileID() { return getAllID(externalUri); }
 
 	public List<String> getAllImagePath() { return getAllPath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI); }
 
 	public List<String> getAllVideoPath() { return getAllPath(MediaStore.Video.Media.EXTERNAL_CONTENT_URI); }
 
 	public List<String> getAllAudioPath() { return getAllPath(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI); }
-	public List<String> getAllFilePath() { return getAllPath(MediaStore.Files.getContentUri("external")); }
+
+	public List<String> getAllFilePath() { return getAllPath(externalUri); }
 
 	public Uri getUriFromID(int id)
 	{
@@ -130,13 +190,13 @@ public class FileManager
 			assert cursor != null;
 			int path_ind = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
 			cursor.moveToFirst();
-			return Uri.fromFile(new File(cursor.getString(path_ind)));
+			return Uri.parse("file://" + cursor.getString(path_ind));
 		}
 	}
 
 	public int getIDFromUri(Uri path)
 	{
-		try (Cursor cursor = contentResolver.query(MediaStore.Files.getContentUri("external"), new String[]{ MediaStore.MediaColumns._ID },
+		try (Cursor cursor = contentResolver.query(externalUri, new String[]{ MediaStore.MediaColumns._ID },
 				MediaStore.MediaColumns.DATA + "=?", new String[]{ path.getPath() }, null))
 		{
 			assert cursor != null;
@@ -144,5 +204,70 @@ public class FileManager
 			cursor.moveToFirst();
 			return cursor.getInt(id_ind);
 		}
+	}
+
+	public String getMimeType(int id)
+	{
+		try (Cursor cursor = contentResolver.query(MediaStore.Files.getContentUri("external", id),
+				new String[]{ MediaStore.MediaColumns.MIME_TYPE }, null, null, null))
+		{
+			assert cursor != null;
+			int mime_ind = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.MIME_TYPE);
+			cursor.moveToFirst();
+			return cursor.getString(mime_ind);
+		}
+	}
+
+	public String getMimeType(Uri uri) { return getMimeType(getIDFromUri(uri)); }
+
+	public Uri stringToUri(String path) { return Uri.parse("file://" + path); }
+
+	/*public Bitmap getThumbnail(String path, int id, boolean isImage)
+	{
+		Bitmap bitmap;
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
+		{
+			try
+			{
+				//bitmap = context.getContentResolver().loadThumbnail(data.uri, new Size(255, 255), null);
+				bitmap = contentResolver.loadThumbnail(ContentUris.withAppendedId(externalUri, id),
+						new Size(384, 512), null);
+			}
+			catch (IOException e)
+			{
+				throw new RuntimeException(e);
+			}
+		}
+		else
+		{
+			if (isImage)
+			{
+				bitmap = MediaStore.Images.Thumbnails.getThumbnail(contentResolver, id, MediaStore.Images.Thumbnails.MINI_KIND, null);
+				if (bitmap == null)
+				{
+					bitmap = ThumbnailUtils.extractThumbnail(BitmapFactory.decodeFile(path), 384, 512);
+				}
+			}
+			else
+			{
+				bitmap = MediaStore.Video.Thumbnails.getThumbnail(contentResolver, id, MediaStore.Video.Thumbnails.MINI_KIND, null);
+				if (bitmap == null)
+				{
+					assert path != null;
+					bitmap = ThumbnailUtils.createVideoThumbnail(path, MediaStore.Video.Thumbnails.MINI_KIND);
+					if (bitmap == null)
+					{
+						bitmap = ThumbnailUtils.extractThumbnail(BitmapFactory.decodeFile(path), 384, 512);
+					}
+				}
+			}
+		}
+		return bitmap;
+	}*/
+
+	public void thumbnailIntoImageView(ImageView imageView, Uri uri)
+	{
+		Glide.with(context).load(uri).set(Downsampler.ALLOW_HARDWARE_CONFIG, true).transition(DrawableTransitionOptions.withCrossFade())
+				.into(imageView);
 	}
 }
--- app/src/main/java/com/cyberegylet/antiDupeGallery/Main.java
+++ app/src/main/java/com/cyberegylet/antiDupeGallery/Main.java
@@ -1,21 +1,175 @@
 package com.cyberegylet.antiDupeGallery;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.cyberegylet.antiDupeGallery.backend.Backend;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class Main extends Activity {
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+class Data
+{
+	public final int id;
+	public final Uri uri;
+	public final String folderName;
+	public final int fileCount;
 
-        setContentView(R.layout.main_activity);
+	Data(Uri uri, int id, int fileCount, String folderName)
+	{
+		this.uri = uri;
+		this.id = id;
+		this.fileCount = fileCount;
+		this.folderName = folderName;
+	}
+}
 
-        TextView a = findViewById(R.id.textView);
-        a.setText(String.valueOf(Backend.test()));
-    }
+class MyAdapter3 extends RecyclerView.Adapter<MyAdapter3.ViewHolder>
+{
+	private final List<Data> datas;
+
+	private final FileManager fileManager;
+
+	public MyAdapter3(List<Data> datas, FileManager fileManager)
+	{
+		this.datas = datas;
+		this.fileManager = fileManager;
+	}
+
+	public static class ViewHolder extends RecyclerView.ViewHolder
+	{
+		public ImageView img;
+		public TextView name;
+		public TextView count;
+
+		public ViewHolder(View itemView)
+		{
+			super(itemView);
+			img = itemView.findViewById(R.id.image);
+			name = itemView.findViewById(R.id.folderName);
+			count = itemView.findViewById(R.id.fileCount);
+		}
+	}
+
+	@NonNull
+	@Override
+	public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+	{
+		View contactView = LayoutInflater.from(parent.getContext()).inflate(R.layout.folder, parent, false);
+		return new ViewHolder(contactView);
+	}
+
+	@Override
+	public void onBindViewHolder(@NonNull MyAdapter3.ViewHolder holder, int position)
+	{
+		Data data = datas.get(position);
+		fileManager.thumbnailIntoImageView(holder.img, data.uri);
+		holder.name.setText(data.folderName);
+		holder.count.setText(String.valueOf(data.fileCount));
+	}
+
+	@Override
+	public int getItemCount()
+	{
+		return datas.size();
+	}
+}
+
+public class Main extends Activity
+{
+	public static void println(String text)
+	{
+		Log.d("App.Main", text);
+	}
+
+	public static void println(Object num) { println(String.valueOf(num)); }
+
+	public static void println() { println(""); }
+
+	private FileManager fileManager;
+	private RecyclerView recyclerView;
+	ArrayList<Data> datas = new ArrayList<>();
+
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.main_activity);
+
+		recyclerView = findViewById(R.id.recycle);
+		findViewById(R.id.downBut2).setOnClickListener(v -> recyclerView.scrollToPosition(datas.size() - 1));
+		findViewById(R.id.upBut2).setOnClickListener(v -> recyclerView.scrollToPosition(0));
+
+		fileManager = new FileManager(this);
+		if (fileManager.hasReadAccess()) fileThings();
+	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+	{
+		if (requestCode == FileManager.STORAGE_REQUEST_CODE && Arrays.stream(grantResults).allMatch(v -> v == 0))
+		{
+			fileThings();
+		}
+		else finishAndRemoveTask();
+	}
+
+	private void fileThings()
+	{
+		HashMap<String, Object[]> folderNames = new HashMap<>();
+
+		FileManager.CursorLoopWrapper wrapper = new FileManager.CursorLoopWrapper()
+		{
+			@Override
+			public void run()
+			{
+				String path = getPath();
+				int id = getID();
+				//if (path.contains("/.")) return; // check if file is in empty directory
+				int lastThing = path.lastIndexOf('/');
+
+				if (lastThing == -1) return; // check if path doesn't have '/' -> some file "can" be in root
+
+				int secondLastThing = path.lastIndexOf('/', lastThing - 1);
+
+				String folderAbs = path.substring(0, lastThing);
+				Object[] tmp = folderNames.get(folderAbs);
+				if (tmp != null)
+				{
+					tmp[1] = (Integer) tmp[1] + 1;
+					return;
+				}
+
+				folderNames.put(folderAbs,
+						new Object[]{ fileManager.stringToUri(path), 1, id, path.substring(secondLastThing + 1, lastThing) });
+			}
+		};
+		String sort = MediaStore.MediaColumns.DATE_MODIFIED + " DESC";
+		fileManager.allImageAndVideoLoop(sort, wrapper, MediaStore.MediaColumns._ID, MediaStore.MediaColumns.DATA);
+
+		Comparator<Object[]> comparator = Comparator.comparing((Object[] a) -> ((String) a[3]));
+		folderNames.entrySet().stream().sorted(Map.Entry.comparingByValue(comparator)).forEach(m -> {
+			Object[] v = m.getValue();
+			datas.add(new Data((Uri) v[0], (Integer) v[2], (Integer) v[1], (String) v[3]));
+		});
+		recyclerView.setAdapter(new MyAdapter3(datas, fileManager));
+
+		findViewById(R.id.load).setVisibility(View.GONE);
+		findViewById(R.id.mainLayout).setClickable(false);
+	}
 }
