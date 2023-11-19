a19e43fe1005a01ebe84fdad9ff25af84f2afcc7
almafa64 <bhatalyak@gmail.com>
2023-11-19 22:37:40 +0100

    Added size, modifiedDate, creationDate fields

--- app/src/main/java/com/cyberegylet/antiDupeGallery/models/Folder.java
+++ app/src/main/java/com/cyberegylet/antiDupeGallery/models/Folder.java
@@ -6,6 +6,11 @@ import androidx.annotation.NonNull;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.Objects;
 
@@ -15,25 +20,49 @@ public class Folder
 	public final Uri path;
 	@NotNull
 	public final String name;
-
-	@NonNull
-	public String getName()
-	{
-		return name;
-	}
+	public final long size;
+	public final long modifiedDate;
+	public final long creationDate;
 
 	public Folder(Uri path)
 	{
 		this.path = path;
 		this.name = Objects.requireNonNull(path.getLastPathSegment());
+		try
+		{
+			Path p = Paths.get(path.getPath());
+			BasicFileAttributes attr = Files.readAttributes(p, BasicFileAttributes.class);
+			size = attr.size();
+			modifiedDate = attr.lastModifiedTime().toMillis();
+			creationDate = attr.creationTime().toMillis();
+		}
+		catch (IOException e)
+		{
+			throw new RuntimeException();
+		}
 	}
 
 	public Folder(Folder folder, boolean copyImages)
 	{
 		this.name = folder.name;
 		this.path = folder.path;
+		this.size = folder.size;
+		this.modifiedDate = folder.modifiedDate;
+		this.creationDate = folder.creationDate;
 		if (copyImages) images.addAll(folder.images);
 	}
 
 	public Folder(Folder folder) { this(folder, false); }
+
+	@NonNull
+	public String getName() { return name; }
+
+	public Uri getPath() { return path; }
+
+	public long getSize() { return size; }
+
+	public long getModifiedDate() { return modifiedDate; }
+
+	public long getCreationDate() { return creationDate; }
+
 }
--- app/src/main/java/com/cyberegylet/antiDupeGallery/models/ImageFile.java
+++ app/src/main/java/com/cyberegylet/antiDupeGallery/models/ImageFile.java
@@ -6,27 +6,57 @@ import android.os.Parcelable;
 
 import androidx.annotation.NonNull;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+
 public class ImageFile implements Parcelable
 {
 	private final Uri path;
 	private final String basename;
+	private final long size;
+	private final long modifiedDate;
+	private final long creationDate;
 
 	public ImageFile(Uri path)
 	{
 		this.path = path;
 		this.basename = path.getLastPathSegment();
+		try
+		{
+			Path p = Paths.get(path.getPath());
+			BasicFileAttributes attr = Files.readAttributes(p, BasicFileAttributes.class);
+			size = attr.size();
+			modifiedDate = attr.lastModifiedTime().toMillis();
+			creationDate = attr.creationTime().toMillis();
+		}
+		catch (IOException e)
+		{
+			throw new RuntimeException();
+		}
 	}
 
 	public Uri getPath() { return path; }
 
 	public String getBasename() { return basename; }
 
+	public long getSize() { return size; }
+
+	public long getModifiedDate() { return modifiedDate; }
+
+	public long getCreationDate() { return creationDate; }
+
 	private ImageFile(Parcel in)
 	{
-		String[] data = new String[2];
+		String[] data = new String[5];
 		in.readStringArray(data);
 		this.path = Uri.parse(data[0]);
 		this.basename = data[1];
+		this.size = Long.parseLong(data[2]);
+		this.modifiedDate = Long.parseLong(data[3]);
+		this.creationDate = Long.parseLong(data[4]);
 	}
 
 	public static final Creator<ImageFile> CREATOR = new Creator<ImageFile>()
@@ -44,6 +74,7 @@ public class ImageFile implements Parcelable
 	@Override
 	public void writeToParcel(@NonNull Parcel dest, int flags)
 	{
-		dest.writeStringArray(new String[]{ path.toString(), basename });
+		dest.writeStringArray(new String[]{ path.toString(), basename, String.valueOf(size), String.valueOf(modifiedDate),
+				String.valueOf(creationDate) });
 	}
 }
