87b21e84186e09a88016841c9a8928a613daa22d
almafa64 <bhatalyak@gmail.com>
2023-11-30 23:33:57 +0100

    Added pin based authentication

--- app/src/main/java/com/cyberegylet/antiDupeGallery/activities/SettingsActivity.java
+++ app/src/main/java/com/cyberegylet/antiDupeGallery/activities/SettingsActivity.java
@@ -2,6 +2,14 @@ package com.cyberegylet.antiDupeGallery.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
@@ -22,12 +30,62 @@ public class SettingsActivity extends Activity
 		SimpleActivityGenerator generator = new SimpleActivityGenerator(this, false, R.string.settings_header);
 
 		generator.newHeader(R.string.settings_general_heading);
-		generator.addRow(R.string.settings_pin, v -> {
-			// ToDo open popup
-			/*View popup = getLayoutInflater().inflate(R.layout.dialog_enter_pin, null);
-			PopupWindow window = new PopupWindow(popup, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
-			window.showAtLocation(v, Gravity.CENTER, 0, 0);*/
+
+		CheckBox checkBox = new CheckBox(this);
+		checkBox.setChecked(ConfigManager.getConfig(ConfigManager.Config.PIN_LOCk).length() != 0);
+		checkBox.setOnCheckedChangeListener((v, check) -> {
+			if(!check)
+			{
+				ConfigManager.setConfig(ConfigManager.Config.PIN_LOCk, "");
+				return;
+			}
+			// ToDo fix dialogue
+			ViewGroup popup = (ViewGroup) getLayoutInflater().inflate(R.layout.dialog_enter_pin, null);
+			TextView text = (TextView) popup.getChildAt(0);
+			text.setText(R.string.pin_enter_pin);
+			PopupWindow window = new PopupWindow(popup, 400, 400, true); // ToDo auto resize
+			window.showAtLocation(v, Gravity.CENTER, 0, 0);
+			popup.setOnTouchListener((v2, e) -> {
+				window.dismiss();
+				v2.performClick();
+				return true;
+			});
+			EditText editText = (EditText) popup.getChildAt(1);
+			final String[] tmpPin = new String[] {""};
+			editText.addTextChangedListener(new TextWatcher()
+			{
+				@Override
+				public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+				@Override
+				public void afterTextChanged(Editable s) { }
+
+				@Override
+				public void onTextChanged(CharSequence s, int start, int before, int count)
+				{
+					if (s.length() == 4)
+					{
+						if(text.getText() == getResources().getText(R.string.pin_enter_pin))
+						{
+							text.setText(R.string.pin_enter_pin_again);
+							tmpPin[0] = s.toString();
+							editText.getText().clear();
+						}
+						else if(s.toString().equals(tmpPin[0]))
+						{
+							ConfigManager.setConfig(ConfigManager.Config.PIN_LOCk, s.toString());
+							window.dismiss();
+						}
+						else
+						{
+							text.setText(R.string.pin_enter_pin);
+							editText.getText().clear();
+						}
+					}
+				}
+			});
 		});
+		generator.addRow(R.string.settings_pin, checkBox);
 		generator.addConfigCheckRow(R.string.settings_show_hidden, ConfigManager.Config.SHOW_HIDDEN);
 		generator.addConfigCheckRow(R.string.settings_use_bin, ConfigManager.Config.USE_BIN);
 
