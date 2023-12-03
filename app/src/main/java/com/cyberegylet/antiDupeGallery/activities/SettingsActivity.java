6a8f6c04418b77fbec03b62b142f390e21e487c9
almafa64 <bhatalyak@gmail.com>
2023-12-03 20:34:41 +0100

    Fixed PIN inptu checkbox checking PIN layout auto size

--- app/src/main/java/com/cyberegylet/antiDupeGallery/activities/SettingsActivity.java
+++ app/src/main/java/com/cyberegylet/antiDupeGallery/activities/SettingsActivity.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.DisplayMetrics;
 import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.PopupWindow;
@@ -33,23 +35,75 @@ public class SettingsActivity extends Activity
 
 		AppCompatCheckBox checkBox = new AppCompatCheckBox(this);
 		checkBox.setChecked(ConfigManager.getConfig(ConfigManager.Config.PIN_LOCk).length() != 0);
-		checkBox.setOnCheckedChangeListener((v, check) -> {
+		checkBox.setOnClickListener(v -> {
+			if (!checkBox.isChecked())
+			{
+				ConfigManager.setConfig(ConfigManager.Config.PIN_LOCk, "");
+				return;
+			}
+			final boolean[] isGood = { false };
+			final String[] tmpPin = new String[]{ "" };
+			ViewGroup popup = (ViewGroup) getLayoutInflater().inflate(R.layout.dialog_enter_pin, (ViewGroup) v.getRootView(), false);
+			TextView text = (TextView) popup.getChildAt(0);
+			text.setText(R.string.pin_enter_pin);
+			popup.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+			DisplayMetrics metrics = this.getResources().getDisplayMetrics();
+			float ratio = ((float)metrics.heightPixels / (float)metrics.widthPixels);
+			PopupWindow window = new PopupWindow(popup, (int)ratio * popup.getMeasuredHeight() / 2, popup.getMeasuredHeight(), true);
+			window.showAtLocation(v, Gravity.CENTER, 0, 0);
+			window.setOnDismissListener(() -> {
+				if(!isGood[0]) checkBox.setChecked(false);
+			});
+			EditText editText = (EditText) popup.getChildAt(1);
+			editText.addTextChangedListener(new TextWatcher()
+			{
+				@Override
+				public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+				@Override
+				public void afterTextChanged(Editable s) { }
+
+				@Override
+				public void onTextChanged(CharSequence s, int start, int before, int count)
+				{
+					if (s.length() == 4)
+					{
+						if (text.getText() == getResources().getText(R.string.pin_enter_pin))
+						{
+							text.setText(R.string.pin_enter_pin_again);
+							tmpPin[0] = s.toString();
+							editText.getText().clear();
+						}
+						else if (s.toString().equals(tmpPin[0]))
+						{
+							ConfigManager.setConfig(ConfigManager.Config.PIN_LOCk, s.toString());
+							isGood[0] = true;
+							window.dismiss();
+						}
+						else
+						{
+							text.setText(R.string.pin_enter_pin);
+							editText.getText().clear();
+						}
+					}
+				}
+			});
+		});
+		/*checkBox.setOnCheckedChangeListener((v, check) -> {
 			if (!check)
 			{
 				ConfigManager.setConfig(ConfigManager.Config.PIN_LOCk, "");
 				return;
 			}
-			// ToDo fix dialogue
-			ViewGroup popup = (ViewGroup) getLayoutInflater().inflate(R.layout.dialog_enter_pin, null);
+			ViewGroup popup = (ViewGroup) getLayoutInflater().inflate(R.layout.dialog_enter_pin, (ViewGroup) v.getRootView(), false);
 			TextView text = (TextView) popup.getChildAt(0);
 			text.setText(R.string.pin_enter_pin);
-			PopupWindow window = new PopupWindow(popup, 400, 400, true); // ToDo auto resize
+			popup.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+			DisplayMetrics metrics = this.getResources().getDisplayMetrics();
+			float ratio = ((float)metrics.heightPixels / (float)metrics.widthPixels);
+			PopupWindow window = new PopupWindow(popup, (int)ratio * popup.getMeasuredHeight(), popup.getMeasuredHeight(), true);
 			window.showAtLocation(v, Gravity.CENTER, 0, 0);
-			popup.setOnTouchListener((v2, e) -> {
-				window.dismiss();
-				v2.performClick();
-				return true;
-			});
+			window.setOnDismissListener(() -> checkBox.setChecked(false));
 			EditText editText = (EditText) popup.getChildAt(1);
 			final String[] tmpPin = new String[]{ "" };
 			editText.addTextChangedListener(new TextWatcher()
@@ -75,6 +129,7 @@ public class SettingsActivity extends Activity
 						{
 							ConfigManager.setConfig(ConfigManager.Config.PIN_LOCk, s.toString());
 							window.dismiss();
+							checkBox.setChecked(true);
 						}
 						else
 						{
@@ -84,7 +139,7 @@ public class SettingsActivity extends Activity
 					}
 				}
 			});
-		});
+		});*/
 		generator.addRow(R.string.settings_pin, checkBox);
 		generator.addConfigCheckRow(R.string.settings_show_hidden, ConfigManager.Config.SHOW_HIDDEN);
 		generator.addConfigCheckRow(R.string.settings_use_bin, ConfigManager.Config.USE_BIN);
