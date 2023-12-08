6c53a70216c6126d3451cff01eaab21bfe9a9c46
almafa64 <bhatalyak@gmail.com>
2023-12-09 00:38:17 +0100

    New class for displaying 1:1 ratio images -> RelativeLayout in image cards (more speeeed)

---
+++ app/src/main/java/com/cyberegylet/antiDupeGallery/views/SquareThumbnailImage.java
@@ -0,0 +1,29 @@
+package com.cyberegylet.antiDupeGallery.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatImageView;
+
+public class SquareThumbnailImage extends AppCompatImageView
+{
+
+	public SquareThumbnailImage(@NonNull Context context) { super(context); }
+
+	public SquareThumbnailImage(@NonNull Context context, @Nullable AttributeSet attrs) { super(context, attrs); }
+
+	public SquareThumbnailImage(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr)
+	{
+		super(
+				context,
+				attrs,
+				defStyleAttr
+		);
+	}
+
+	/**  */
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) { super.onMeasure(widthMeasureSpec, widthMeasureSpec); }
+}
