b010f8a19a0731fbe43f986a43043ab7b05de42a
almafa64 <bhatalyak@gmail.com>
2023-12-29 02:54:21 +0100

    Added code for converting bytes to String

--- app/src/main/java/com/cyberegylet/antiDupeGallery/backend/Utils.java
+++ app/src/main/java/com/cyberegylet/antiDupeGallery/backend/Utils.java
@@ -1,14 +1,22 @@
 package com.cyberegylet.antiDupeGallery.backend;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Utils
 {
 	public static String getByteStringFromSize(long size)
 	{
-		final float kb = 1024;
-		if(size >= kb * kb * kb * kb) return (size / (kb * kb * kb * kb)) + " TB";
-		else if(size >= kb * kb * kb) return (size / (kb * kb * kb)) + " GB";
-		else if(size >= kb * kb) return (size / (kb * kb)) + " MB";
-		else if(size >= kb) return (size / kb) + " KB";
-		return (float)size + " B";
+		final double kb = 1024;
+		if(size >= kb * kb * kb * kb) return doubleString(size / (kb * kb * kb * kb)) + " TB";
+		else if(size >= kb * kb * kb) return doubleString(size / (kb * kb * kb)) + " GB";
+		else if(size >= kb * kb) return doubleString(size / (kb * kb)) + " MB";
+		else if(size >= kb) return doubleString(size / kb) + " KB";
+		return doubleString((double) size) + " B";
+	}
+
+	private static String doubleString(double d)
+	{
+		return BigDecimal.valueOf(d).setScale(2, RoundingMode.HALF_UP).toPlainString() + "";
 	}
 }
