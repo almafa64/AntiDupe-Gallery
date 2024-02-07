b9ce0dc06bb2e873ab3d25a305c346e09eb4cd95
almafa64 <bhatalyak@gmail.com>
2024-02-07 20:28:47 +0100

    Extended MyAsyncTask functionalities

--- app/src/main/java/com/cyberegylet/antiDupeGallery/helpers/MyAsyncTask.kt
+++ app/src/main/java/com/cyberegylet/antiDupeGallery/helpers/MyAsyncTask.kt
@@ -2,16 +2,28 @@ package com.cyberegylet.antiDupeGallery.helpers
 
 abstract class MyAsyncTask
 {
+	private var shouldStop: Boolean = false
+	var thread: Thread? = null
+		private set
+
 	fun execute()
 	{
-		Thread {
+		thread = Thread {
 			onPreExecute()
-			doInBackground()
-			onPostExecute()
-		}.start()
+			if(!shouldStop) doInBackground()
+			if(!shouldStop) onPostExecute()
+		}
+		thread?.start()
 	}
 
 	abstract fun doInBackground()
 	abstract fun onPostExecute()
 	abstract fun onPreExecute()
+
+	/**
+	 * sets thread stop variable, which will block remaining steps
+	 */
+	fun stop() { shouldStop = true }
+
+	fun running(): Boolean = thread?.isAlive ?: false
 }
