1948830b0bc17d2ad7222457d52862380c873e4e
almafa64 <bhatalyak@gmail.com>
2024-02-09 20:12:52 +0100

    Made jetpack compose test

--- app/src/main/java/com/cyberegylet/antiDupeGallery/activities/AboutActivity.kt
+++ app/src/main/java/com/cyberegylet/antiDupeGallery/activities/AboutActivity.kt
@@ -1,27 +1,65 @@
-package com.cyberegylet.antiDupeGallery.activities;
+package com.cyberegylet.antiDupeGallery.activities
 
-import android.app.Activity;
-import android.os.Bundle;
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.cyberegylet.antiDupeGallery.R
+import com.cyberegylet.antiDupeGallery.activities.ui.theme.AntiDupeGalleryTheme
 
-import androidx.annotation.Nullable;
-
-import com.cyberegylet.antiDupeGallery.R;
-import com.cyberegylet.antiDupeGallery.helpers.SimpleActivityGenerator;
-import com.cyberegylet.antiDupeGallery.helpers.activities.ActivityManager;
-
-public class AboutActivity extends Activity
+class AboutActivity : AppCompatActivity()
 {
-	@Override
-	protected void onCreate(@Nullable Bundle savedInstanceState)
+	override fun onCreate(savedInstanceState: Bundle?)
 	{
-		super.onCreate(savedInstanceState);
+		super.onCreate(savedInstanceState)
+		/*val generator = SimpleActivityGenerator(this, true, R.string.about_header_text)
+		generator.newHeader(R.string.about_support)
+		generator.addRow(R.string.email, R.drawable.ic_email)
+		generator.newHeader(R.string.about_other)
+		generator.addRow(R.string.version_text, R.drawable.ic_info)
+		findViewById<View>(R.id.back_button).setOnClickListener { v: View? -> goBack(this@AboutActivity) }*/
 
-		SimpleActivityGenerator generator = new SimpleActivityGenerator(this, true, R.string.about_header_text);
-		generator.newHeader(R.string.about_support);
-		generator.addRow(R.string.email, R.drawable.ic_email);
-		generator.newHeader(R.string.about_other);
-		generator.addRow(R.string.version_text, R.drawable.ic_info);
+		setContent {
+			AntiDupeGalleryTheme {
+				Surface(modifier = Modifier.fillMaxWidth(), color = MaterialTheme.colorScheme.background) {
+					ShowText(txt = "test")
+				}
+			}
+		}
+	}
 
-		findViewById(R.id.back_button).setOnClickListener(v -> ActivityManager.goBack(AboutActivity.this));
+	@Composable
+	fun ShowText(txt: String)
+	{
+		Text(text = txt)
+	}
+
+	@Preview(
+		name = "Light",
+		showBackground = true,
+		showSystemUi = true,
+	)
+	@Preview(
+		name = "Dark",
+		showBackground = true,
+		showSystemUi = true,
+		uiMode = Configuration.UI_MODE_NIGHT_YES
+	)
+	@Composable
+	fun Preview()
+	{
+		AntiDupeGalleryTheme {
+			Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+				ShowText(txt = "preview")
+			}
+		}
 	}
 }
---
+++ app/src/main/java/com/cyberegylet/antiDupeGallery/activities/OldAboutActivity.java
@@ -0,0 +1,27 @@
+package com.cyberegylet.antiDupeGallery.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.cyberegylet.antiDupeGallery.R;
+import com.cyberegylet.antiDupeGallery.helpers.SimpleActivityGenerator;
+import com.cyberegylet.antiDupeGallery.helpers.activities.ActivityManager;
+
+public class OldAboutActivity extends Activity
+{
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+
+		SimpleActivityGenerator generator = new SimpleActivityGenerator(this, true, R.string.about_header_text);
+		generator.newHeader(R.string.about_support);
+		generator.addRow(R.string.email, R.drawable.ic_email);
+		generator.newHeader(R.string.about_other);
+		generator.addRow(R.string.version_text, R.drawable.ic_info);
+
+		findViewById(R.id.back_button).setOnClickListener(v -> ActivityManager.goBack(OldAboutActivity.this));
+	}
+}
